5311811c7ecfb81db69b9f177b2ee3074e378d0f
POlaNasserSamouel <121816986+POlaNasserSamouel@users.noreply.github.com>
2026-09-11 12:25:46 +0300

    hjhhh

--- obj/Debug/net10.0/PharmacyAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/PharmacyAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmacyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0+28a19e9aeb5c311e1310aeea15806a07f097d903")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0+b6bdcc013f170e357a0051dd6738ef3b69ae9618")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmacyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmacyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
